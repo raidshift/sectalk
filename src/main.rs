e0458c865b7ae0b5f9d5df861674b8d11594c509
raidshift <raidshift.x@gmail.com>
2025-04-17 21:10:10 +0200

    1.0.2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use once_cell::sync::Lazy;
 use rand_chacha::ChaCha20Rng;
 use rand_core::{RngCore, SeedableRng};
 use secp256k1::{self, PublicKey, Secp256k1};
+use std::cmp::Ordering;
 use std::collections::HashMap;
 use std::hash::{DefaultHasher, Hash, Hasher};
 use std::sync::Arc;
@@ -25,7 +26,13 @@ struct Msg(pub [u8; WS_MSG_LEN]);
 #[derive(PartialEq, Eq, Debug, Clone, Copy)]
 struct RoomKey(u64);
 struct Room {
-    peer_txs: Vec<Arc<Mutex<SplitSink<WebSocket, Message>>>>,
+    peer_a_tx: Option<Arc<Mutex<SplitSink<WebSocket, Message>>>>,
+    peer_b_tx: Option<Arc<Mutex<SplitSink<WebSocket, Message>>>>,
+}
+
+enum Peer {
+    A,
+    B,
 }
 struct Rooms(HashMap<RoomKey, Arc<Mutex<Room>>>);
 
@@ -51,25 +58,40 @@ impl Hash for RoomKey {
 
 impl Room {
     pub fn new() -> Self {
-        Room { peer_txs: Vec::new() }
+        Room {
+            peer_a_tx: None,
+            peer_b_tx: None,
+        }
     }
 
-    pub fn add_tx(&mut self, tx: Arc<Mutex<SplitSink<WebSocket, Message>>>) {
-        self.peer_txs.push(tx);
+    pub fn get_peer_tx(&mut self, peer: &Peer) -> &mut Option<Arc<Mutex<SplitSink<WebSocket, Message>>>> {
+        return match peer {
+            Peer::A => &mut self.peer_a_tx,
+            Peer::B => &mut self.peer_b_tx,
+        };
     }
 
-    pub fn remove_tx(&mut self, tx: Arc<Mutex<SplitSink<WebSocket, Message>>>) -> usize {
-        self.peer_txs.remove(
-            self.peer_txs
-                .iter()
-                .position(|tx_in_room: &Arc<Mutex<SplitSink<WebSocket, Message>>>| Arc::ptr_eq(&tx_in_room, &tx))
-                .unwrap(),
-        );
-        self.peer_txs.len()
+    pub async fn add_peer(&mut self, tx: Arc<Mutex<SplitSink<WebSocket, Message>>>, peer: &Peer, id: &Uuid) {
+        let peer_tx = self.get_peer_tx(peer);
+
+        if let Some(peer_tx) = peer_tx {
+            let mut tx_guard = peer_tx.lock().await;
+            send(&mut tx_guard, id, &[0x00]).await;
+        }
+
+        *peer_tx = Some(tx);
     }
 
-    pub fn peer_txs(&self) -> impl Iterator<Item = Arc<Mutex<SplitSink<WebSocket, Message>>>> + '_ {
-        self.peer_txs.iter().cloned()
+    pub fn remove_peer(&mut self, peer: &Peer) {
+        *self.get_peer_tx(peer) = None;
+    }
+
+    pub fn has_peers(&self) -> bool {
+        if self.peer_b_tx.is_none() && self.peer_b_tx.is_none() {
+            true
+        } else {
+            false
+        }
     }
 }
 
@@ -78,14 +100,14 @@ impl Rooms {
         Rooms(HashMap::new())
     }
 
-    pub fn enroll(&mut self, room_key: RoomKey) -> Arc<Mutex<Room>> {
+    pub fn add_room(&mut self, room_key: RoomKey) -> Arc<Mutex<Room>> {
         self.0
             .entry(room_key)
             .or_insert_with(|| Arc::new(Mutex::new(Room::new())))
             .clone()
     }
 
-    pub fn remove(&mut self, room_key: RoomKey) {
+    pub fn remove_room(&mut self, room_key: RoomKey) {
         self.0.remove(&room_key);
     }
 }
@@ -141,6 +163,7 @@ async fn handle_session(ws: WebSocket) {
     let mut room: Option<Arc<Mutex<Room>>> = None;
     let mut room_key: Option<RoomKey> = None;
     let mut verify_sig_msg: [u8; SIG_MSG_LEN] = [0u8; SIG_MSG_LEN];
+    let mut this_peer: &Option<Peer> = &None;
 
     let mut rng = ChaCha20Rng::from_os_rng();
     rng.fill_bytes(&mut verify_sig_msg);
@@ -160,31 +183,7 @@ async fn handle_session(ws: WebSocket) {
 
         match room {
             None => {
-                if ws_message_bytes.len() == 2 * PUB_KEY_LEN + SIG_LEN {
-                    let pka: [u8; PUB_KEY_LEN] = ws_message_bytes[..PUB_KEY_LEN].try_into().unwrap();
-                    let pkb: [u8; PUB_KEY_LEN] = ws_message_bytes[PUB_KEY_LEN..2 * PUB_KEY_LEN].try_into().unwrap();
-
-                    if !verify_signature(&pka, &ws_message_bytes[2 * PUB_KEY_LEN..], &verify_sig_msg) {
-                        debug!("{:?}: Authentication failed", session_id);
-                        break;
-                    }
-
-                    room_key = Some(RoomKey::new(&pka, &pkb));
-                    {
-                        let mut rooms_guard = ROOMS.lock().await;
-                        room = Some(rooms_guard.enroll(room_key.unwrap()));
-                    }
-
-                    {
-                        let mut room_guard = room.as_ref().unwrap().lock().await;
-                        room_guard.add_tx(tx.clone());
-                    }
-
-                    debug!("{:?}: Enrolled in room: {:?}", session_id, &room_key.unwrap().0);
-
-                    let mut tx_guard = tx.lock().await;
-                    send(&mut tx_guard, &session_id, &room_key.unwrap().0.to_be_bytes()).await;
-                } else {
+                if ws_message_bytes.len() != 2 * PUB_KEY_LEN + SIG_LEN {
                     debug!(
                         "{:?}: Message length: {} expected: {}",
                         session_id,
@@ -193,17 +192,46 @@ async fn handle_session(ws: WebSocket) {
                     );
                     break;
                 }
+
+                let pka: [u8; PUB_KEY_LEN] = ws_message_bytes[..PUB_KEY_LEN].try_into().unwrap();
+                let pkb: [u8; PUB_KEY_LEN] = ws_message_bytes[PUB_KEY_LEN..2 * PUB_KEY_LEN].try_into().unwrap();
+
+                this_peer = match pka.cmp(&pkb) {
+                    Ordering::Less => &Some(Peer::A),
+                    Ordering::Greater => &Some(Peer::B),
+                    _ => &None,
+                };
+
+                if this_peer.is_none() {
+                    debug!("{:?}: Peers must be different", session_id);
+                    break;
+                }
+
+                if !verify_signature(&pka, &ws_message_bytes[2 * PUB_KEY_LEN..], &verify_sig_msg) {
+                    debug!("{:?}: Authentication failed", session_id);
+                    break;
+                }
+
+                room_key = Some(RoomKey::new(&pka, &pkb));
+                {
+                    let mut rooms_guard = ROOMS.lock().await;
+                    room = Some(rooms_guard.add_room(room_key.unwrap()));
+                }
+
+                {
+                    let mut room_guard = room.as_ref().unwrap().lock().await;
+                    room_guard
+                        .add_peer(tx.clone(), this_peer.as_ref().unwrap(), &session_id)
+                        .await;
+                }
+
+                debug!("{:?}: Enrolled in room: {:?}", session_id, &room_key.unwrap().0);
+
+                let mut tx_guard = tx.lock().await;
+                send(&mut tx_guard, &session_id, &room_key.unwrap().0.to_be_bytes()).await;
             }
             Some(ref room) => {
-                if ws_message_bytes.len() == WS_MSG_LEN {
-                    let message = Msg(ws_message_bytes.try_into().unwrap());
-
-                    let room_guard = room.lock().await;
-                    for peer_tx in room_guard.peer_txs() {
-                        let mut peer_tx_guard = peer_tx.lock().await;
-                        send(&mut peer_tx_guard, &session_id, &message.0).await;
-                    }
-                } else {
+                if ws_message_bytes.len() != WS_MSG_LEN {
                     debug!(
                         "{:?}: Message length: {} expected: {}",
                         session_id,
@@ -212,19 +240,49 @@ async fn handle_session(ws: WebSocket) {
                     );
                     break;
                 }
+
+                if this_peer.is_none() {
+                    error!("{:?}: Peer not set", session_id);
+                    break;
+                }
+
+                let message = Msg(ws_message_bytes.try_into().unwrap());
+
+                let this_peer = this_peer.as_ref().unwrap();
+
+                let other_peer = match this_peer {
+                    Peer::A => &Peer::B,
+                    Peer::B => &Peer::A,
+                };
+
+                let mut room_guard = room.lock().await;
+
+                if let Some(peer_tx) = room_guard.get_peer_tx(this_peer) {
+                    let mut peer_tx_guard = peer_tx.lock().await;
+                    send(&mut peer_tx_guard, &session_id, &message.0).await;
+                }
+
+                if let Some(peer_tx) = room_guard.get_peer_tx(other_peer) {
+                    let mut peer_tx_guard = peer_tx.lock().await;
+                    send(&mut peer_tx_guard, &session_id, &message.0).await;
+                }
             }
         }
     }
 
     if let Some(room) = room {
         let mut room_guard = room.lock().await;
-        if room_guard.remove_tx(tx.clone()) == 0 {
+        if let Some(this_peer) = this_peer {
+            room_guard.remove_peer(this_peer);
+        }
+        if !room_guard.has_peers() {
             if let Some(room_key) = room_key {
                 let mut rooms_guard = ROOMS.lock().await;
-                rooms_guard.remove(room_key);
+                rooms_guard.remove_room(room_key);
                 debug!("{:?}: Removed room: {:?}", session_id, &room_key.0);
             }
         }
     }
+
     info!("{:?}: Session closed", session_id);
 }
